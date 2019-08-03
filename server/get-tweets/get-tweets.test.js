f92c5df4b4a9daa1d545e87bf5aee04c8377b986
Brendan Brown <brendanbrown27@gmail.com>
2019-08-03 10:29:34 -0700

    improve get tweet test structure

--- server/get-tweets/get-tweets.test.js
+++ server/get-tweets/get-tweets.test.js
@@ -1,24 +1,95 @@
+const moment = require('moment');
 const getTweets = require('./index');
 
+moment.suppressDeprecationWarnings = true;
+
 describe('get tweets', () => {
-  test('successfully hits the twitter api and parses the response', (done) => {
-    getTweets('realdonaldtrump', (error, tweets) => {
-      expect(error).toBeNull();
-      expect(tweets.length > 10).toEqual(true);
+  let error;
+  let tweets;
+  let tweet;
 
-      const tweet = tweets[0];
-
-      expect(typeof tweet.id).toEqual('string');
-      expect(typeof tweet.favorites).toEqual('number');
-      expect(typeof tweet.retweets).toEqual('number');
-      expect(typeof tweet.isRetweet).toEqual('boolean');
-      expect(typeof tweet.text).toEqual('string');
-      expect(typeof tweet.device).toEqual('string');
-      expect(typeof tweet.date).toEqual('number');
-
-      expect(tweet.text.length > 0).toEqual(true);
-      expect(tweet.device.length > 0).toEqual(true);
+  beforeAll((done) => {
+    getTweets('realdonaldtrump', (responseError, responseData) => {
+      error = responseError;
+      tweets = responseData;
+      tweet = responseData[0];
       done();
+    });
+  });
+
+  describe('hitting the twitter API', () => {
+    it('does not error', () => {
+      expect(error).toBeNull();
+    });
+
+    it('returns a list of tweets', () => {
+      expect(Array.isArray(tweets)).toEqual(true);
+    });
+
+    it('returns a list of approximately 100 tweets', () => {
+      expect(tweets.length).toBeGreaterThanOrEqual(99);
+    });
+  });
+
+  describe('parsing the response from twitter', () => {
+    describe('the date value (the date the tweet was sent)', () => {
+      it('is a number', () => {
+        expect(typeof tweet.date).toEqual('number');
+      });
+
+      it('is a valid JS timestamp', () => {
+        const year = new Date(tweet.date).getFullYear();
+        expect(year).toBeGreaterThan(2018);
+        expect(year).toBeLessThan(2024);
+      });
+    });
+
+    describe('the device value (the device the tweet was sent from)', () => {
+      it('is a string', () => {
+        expect(typeof tweet.device).toEqual('string');
+      });
+
+      it('has some value', () => {
+        expect(tweet.device.length).toBeGreaterThan(3);
+      });
+    });
+
+    describe('the favorites value (the number of times the tweet has been favorited)', () => {
+      it('is a number', () => {
+        expect(typeof tweet.favorites).toEqual('number');
+      });
+    });
+
+    describe('the id value (the id of the tweet used for linking)', () => {
+      it('is a string', () => {
+        expect(typeof tweet.id).toEqual('string');
+      });
+
+      it('has some value', () => {
+        expect(tweet.device.length).toBeGreaterThan(0);
+      });
+    });
+
+    describe('the isRetweet value (whether the tweet is an original or a retweet)', () => {
+      it('is a boolean', () => {
+        expect(typeof tweet.isRetweet).toEqual('boolean');
+      });
+    });
+
+    describe('the retweets value (the number of times the tweet has been retweeted)', () => {
+      it('is a number', () => {
+        expect(typeof tweet.retweets).toEqual('number');
+      });
+    });
+
+    describe('the text value (the text of the tweet)', () => {
+      it('is a string', () => {
+        expect(typeof tweet.text).toEqual('string');
+      });
+
+      it('has some value', () => {
+        expect(tweet.device.length).toBeGreaterThan(0);
+      });
     });
   });
 });
