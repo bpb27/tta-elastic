ee5d92ddba608e910e6a8b779e59f450bb0e2810
Brendan Brown <brendanbrown27@gmail.com>
2019-09-19 21:09:33 -0700

    <EmbeddedTweet/> and <TweetLink/>

---
+++ src/tweet-link/tweet-link.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TweetLink from './tweet-link.component';
+
+const createProps = () => ({
+  children: null,
+  id: '1',
+  text: 'marshmellow cannon'
+});
+
+describe('TweetLink', () => {
+  test('link to the tweet', () => {
+    const props = createProps();
+    const wrapper = shallow(<TweetLink {...props} />);
+    const url = wrapper.find('a').props().href;
+    expect(url).toEqual('https://twitter.com/realDonaldTrump/status/1');
+  });
+
+  test('renders text', () => {
+    const props = createProps();
+    const wrapper = shallow(<TweetLink {...props} />);
+    const text = wrapper.find('a').text();
+    expect(text).toEqual('"marshmellow cannon"');
+  });
+
+  test('renders children if passed (instead of text)', () => {
+    const props = createProps();
+    const wrapper = shallow(<TweetLink {...props}><h1>Smore</h1></TweetLink>);
+    const children = wrapper.find('h1');
+    expect(children.exists()).toEqual(true);
+  });
+});
