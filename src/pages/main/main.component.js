cbed1a3db1644887ae65d949bc64f723a8c6f83a
Brendan Brown <brendanbrown27@gmail.com>
2019-11-20 21:20:01 -0800

    rep endorsements

--- src/pages/main/main.component.js
+++ src/pages/main/main.component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import LatestTweets from 'components/latest-tweets';
 import ListDog from 'components/lists/dog';
-import ListHypocrisy from 'components/lists/hypocrisy';
 import ListStaff from 'components/lists/staff';
 import './main.style.scss';
 
@@ -9,8 +8,7 @@ export default class Main extends React.Component {
   render () {
     return (
       <div id="main-page">
-        {/* <LatestTweets/> */}
-        <ListHypocrisy/>
+        <LatestTweets/>
         <ListDog/>
         <ListStaff/>
       </div>
