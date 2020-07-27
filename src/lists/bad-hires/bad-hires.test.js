5edb0db7588bc885c61d62933fa0b028b84d5184
Brendan Brown <brendan@arcadiapower.com>
2020-07-26 17:07:48 -0700

    PR review updates

--- src/lists/bad-hires/bad-hires.test.js
+++ src/lists/bad-hires/bad-hires.test.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BadHires from './bad-hires.component';
 
-const createProps = () => ({
-
-});
-
 describe('BadHires', () => {
   it('renders', () => {
-    const props = createProps();
-    const wrapper = shallow(<BadHires {...props} />);
+    const wrapper = shallow(<BadHires />);
     const element = wrapper.find('.badHires');
     expect(element.exists()).toEqual(true);
+  });
+
+  it('clicking the button expands the content', () => {
+    const wrapper = shallow(<BadHires />);
+    expect(wrapper.find('.more').exists()).toEqual(false);
+    wrapper.find('Button').simulate('click');
+    expect(wrapper.find('.more').exists()).toEqual(true);
   });
 });
