6227cb3d215ced7b9df11a2ab0d2847a22779a58
Brendan Brown <brendan@arcadiapower.com>
2020-08-17 20:10:11 -0700

    tests

--- src/pages/collections/post/paragraph/paragraph.test.js
+++ src/pages/collections/post/paragraph/paragraph.test.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Paragraph from './paragraph.component';
 
-const createProps = () => ({
-
-});
-
 describe('Paragraph', () => {
   it('renders', () => {
-    const props = createProps();
-    const wrapper = shallow(<Paragraph {...props}/>);
+    const wrapper = shallow(<Paragraph>Hello</Paragraph>);
     const element = wrapper.find('.paragraph');
+    expect(element.exists()).toEqual(true);
+  });
+
+  it('renders conclusion type', () => {
+    const wrapper = shallow(<Paragraph type="conclusion">Hello</Paragraph>);
+    const element = wrapper.find('.conclusion');
+    expect(element.exists()).toEqual(true);
+  });
+
+  it('renders quote type', () => {
+    const wrapper = shallow(<Paragraph type="quote">Hello</Paragraph>);
+    const element = wrapper.find('.quote');
     expect(element.exists()).toEqual(true);
   });
 });
--- src/pages/main/index.js
+++
@@ -1,3 +0,0 @@
-import Main from './main.component';
-
-export default Main;
