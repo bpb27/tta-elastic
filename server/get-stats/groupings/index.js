c692182a1ea5bd2d294fd19df184b9214e1c0d9c
Brendan Brown <brendan@arcadiapower.com>
2020-09-29 16:07:08 -0700

    add tweets by hour to stats response

--- server/get-stats/groupings/index.js
+++ server/get-stats/groupings/index.js
@@ -6,11 +6,21 @@ const tweetsGroupedBy = unit => `
   group by 1
 `;
 
+const tweetsByHour = () => `
+  select
+  extract(hour from date::timestamp AT time zone 'EST') as date,
+  count(1)
+  from tweets
+  group by 1
+`;
+
 const getGroupings = async pool => {
+  const byHour = await pool.query(tweetsByHour());
   const byDay = await pool.query(tweetsGroupedBy('day'));
   const byWeek = await pool.query(tweetsGroupedBy('week'));
   const byMonth = await pool.query(tweetsGroupedBy('month'));
   return {
+    byHour: byHour.rows,
     byDay: byDay.rows,
     byMonth: byMonth.rows,
     byWeek: byWeek.rows,
