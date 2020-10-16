25f879aacd0c0efcaff4bed706ea6b522d472d34
Brendan Brown <brendan@arcadiapower.com>
2020-10-15 18:36:36 -0700

    fix test

--- src/pages/insights/past/past.test.js
+++ src/pages/insights/past/past.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Past from './past.component';
+import { Past } from './past.component';
+
+const createProps = () => ({
+  history: {
+    push: jest.fn(),
+  },
+});
 
 describe('Past', () => {
   it('renders', () => {
-    const wrapper = shallow(<Past/>);
+    const props = createProps();
+    const wrapper = shallow(<Past {...props}/>);
     const element = wrapper.find('.page');
     expect(element.exists()).toEqual(true);
   });
