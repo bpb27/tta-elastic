8c4aa7f570cdd14c92f31e7352c1f39a35fb64ef
Brendan Brown <brendan@arcadiapower.com>
2020-10-20 17:29:26 -0700

    fix embedcolumn test

--- src/embedded-tweet-column/embedded-tweet-column.test.js
+++ src/embedded-tweet-column/embedded-tweet-column.test.js
@@ -3,14 +3,43 @@ import { shallow } from 'enzyme';
 import EmbeddedTweetColumn from './embedded-tweet-column.component';
 
 const createProps = () => ({
-
+  children: <p>Hello</p>,
+  header: 'Sup',
 });
 
 describe('EmbeddedTweetColumn', () => {
-  it('renders', () => {
-    const props = createProps();
-    const wrapper = shallow(<EmbeddedTweetColumn {...props}/>);
-    const element = wrapper.find('.embeddedTweetColumn');
-    expect(element.exists()).toEqual(true);
+  let props;
+  let wrapper;
+
+
+
+  describe('renders', () => {
+    beforeEach(() => {
+      props = createProps();
+      wrapper = shallow(<EmbeddedTweetColumn {...props}/>);
+    });
+
+    test('container', () => {
+      expect(wrapper.find('.embeddedTweetColumn').exists()).toEqual(true);
+    });
+
+    test('children', () => {
+      expect(wrapper.find('p').exists()).toEqual(true);
+    });
+  });
+
+  describe('header', () => {
+    it('renders if prop passed', () => {
+      props = { ...createProps(), header: 'Hello' };
+      wrapper = shallow(<EmbeddedTweetColumn {...props}/>);
+      expect(wrapper.find('h1').exists()).toEqual(true);
+      expect(wrapper.find('h1').text()).toEqual(props.header);
+    });
+
+    it('does not render if no prop passed', () => {
+      props = { ...createProps(), header: undefined };
+      wrapper = shallow(<EmbeddedTweetColumn {...props}/>);
+      expect(wrapper.find('h1').exists()).toEqual(false);
+    });
   });
 });
