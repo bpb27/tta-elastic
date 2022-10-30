39c0664c74d337114a06802fd5bc061c1b61681c
Brendan Brown <brendan@arcadiapower.com>
2022-10-30 08:42:40 -0700

    update getTweets to new twitter API

--- scripts/twitter-api/index.js
+++ scripts/twitter-api/index.js
@@ -1,79 +1,79 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const request = require('request');
-const Twitter = require('twitter');
-const { Client } = require('elasticsearch');
-const { Pool } = require('pg');
-const uploadToES = require('./upload-to-es');
-const uploadToPG = require('./upload-to-pg');
-const {
-  DATABASE_URL,
-  SEARCHBOX_URL,
-  TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET,
-} = process.env;
+const request = require("request");
+const Twitter = require("twitter");
+const { Client } = require("elasticsearch");
+const { Pool } = require("pg");
+const uploadToES = require("./upload-to-es");
+const uploadToPG = require("./upload-to-pg");
+const { DATABASE_URL, SEARCHBOX_URL, TWITTER_API_KEY, TWITTER_API_KEY_SECRET } =
+  process.env;
 
-const dbString = value => {
-  const ssl = '?ssl=true&sslmode=require';
+const dbString = (value) => {
+  const ssl = "?ssl=true&sslmode=require";
   return value.includes(ssl) ? value : `${value}${ssl}`;
 };
 
 const client = new Client({ host: SEARCHBOX_URL });
 const pool = new Pool({ connectionString: dbString(DATABASE_URL) });
 
-function getTweets (screenName, callback) {
-  const credentials = new Buffer(`${TWITTER_CONSUMER_KEY}:${TWITTER_CONSUMER_SECRET}`).toString('base64');
+function getTweets(screenName, callback) {
+  const credentials = Buffer.from(
+    `${TWITTER_API_KEY}:${TWITTER_API_KEY_SECRET}`
+  ).toString("base64");
   const requestParams = {
-    body: 'grant_type=client_credentials',
+    body: "grant_type=client_credentials",
     headers: {
       Authorization: `Basic ${credentials}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
-    method: 'POST',
-    url: 'https://api.twitter.com/oauth2/token',
+    method: "POST",
+    url: "https://api.twitter.com/oauth2/token",
   };
 
   request(requestParams, (error, response, body) => {
     if (error) return callback(error, null);
 
-    const bearerToken = JSON.parse(body)['access_token'];
+    const bearerToken = JSON.parse(body)["access_token"];
     const client = new Twitter({
-      consumer_key: TWITTER_CONSUMER_KEY,
-      consumer_secret: TWITTER_CONSUMER_SECRET,
-      bearer_token: bearerToken
+      consumer_key: TWITTER_API_KEY,
+      consumer_secret: TWITTER_API_KEY_SECRET,
+      bearer_token: bearerToken,
     });
 
-    const params = {
-      count: 5,
-      screen_name: screenName,
-      trim_user: true,
-      tweet_mode: 'extended',
-    };
+    // find user id https://tweeterid.com/
+    // docs https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets#tab2
 
-    client.get('statuses/user_timeline', params, (error, tweets) => {
+    const apiUrl =
+      "https://api.twitter.com/2/users/25073877/tweets?max_results=100&tweet.fields=created_at%2Cpublic_metrics%2Csource&expansions=referenced_tweets.id&media.fields=public_metrics";
+
+    client.get(apiUrl, {}, (error, tweets) => {
       if (error) return callback(error, null);
 
-      const newTweets = tweets.reduce((hash, tweet) => {
-        hash[tweet.id_str] = {
+      const newTweets = tweets.data.reduce((hash, tweet) => {
+        hash[tweet.id] = {
           date: tweet.created_at,
-          device: tweet.source.split('>')[1].split('<')[0],
-          favorites: tweet.favorite_count,
-          id: tweet.id_str,
+          device: tweet.source,
+          favorites: tweet.public_metrics.like_count || 0,
+          id: tweet.id,
           isDeleted: false,
-          isRetweet: (tweet.full_text || tweet.text).substr(0,3) === 'RT ',
-          retweets: tweet.retweet_count,
-          text: tweet.full_text || tweet.text,
+          isRetweet: !!(
+            tweet.referenced_tweets &&
+            tweet.referenced_tweets.find((t) => t.type === "quoted")
+          ),
+          retweets: tweet.public_metrics.retweet_count || 0,
+          text: tweet.text,
         };
         return hash;
       }, {});
 
       callback(null, Object.values(newTweets));
     });
-
   });
 }
 
-getTweets('realdonaldtrump', (error, data) => {
-  if (error) return console.error('TWITTER FETCH ERROR', error);
-  uploadToPG(pool, data, tweetsFromPG => uploadToES(client, data, tweetsFromPG));
+getTweets("realdonaldtrump", (error, data) => {
+  if (error) return console.error("TWITTER FETCH ERROR", error);
+  if (data) console.log("TWITTER FETCH SUCCESS", data);
+  // uploadToPG(pool, data, tweetsFromPG => uploadToES(client, data, tweetsFromPG));
 });
--- server/get-tweets.js
+++ server/get-tweets.js
@@ -1,59 +1,62 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const request = require('request');
-const Twitter = require('twitter');
-const messages = require('./messages');
-const { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET } = process.env;
+const request = require("request");
+const Twitter = require("twitter");
+const messages = require("./messages");
+const { TWITTER_API_KEY, TWITTER_API_KEY_SECRET } = process.env;
 
-function getTweets (screenName, callback) {
-  const credentials = new Buffer(`${TWITTER_CONSUMER_KEY}:${TWITTER_CONSUMER_SECRET}`).toString('base64');
+function getTweets(screenName, callback) {
+  const credentials = Buffer.from(
+    `${TWITTER_API_KEY}:${TWITTER_API_KEY_SECRET}`
+  ).toString("base64");
   const requestParams = {
-    body: 'grant_type=client_credentials',
+    body: "grant_type=client_credentials",
     headers: {
       Authorization: `Basic ${credentials}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
     },
-    method: 'POST',
-    url: 'https://api.twitter.com/oauth2/token',
+    method: "POST",
+    url: "https://api.twitter.com/oauth2/token",
   };
 
   request(requestParams, (error, response, body) => {
-    if (error) return callback({ error, message: messages.fetches.twBearer }, null);
+    if (error)
+      return callback({ error, message: messages.fetches.twBearer }, null);
 
-    const bearerToken = JSON.parse(body)['access_token'];
+    const bearerToken = JSON.parse(body)["access_token"];
     const client = new Twitter({
-      consumer_key: TWITTER_CONSUMER_KEY,
-      consumer_secret: TWITTER_CONSUMER_SECRET,
-      bearer_token: bearerToken
+      consumer_key: TWITTER_API_KEY,
+      consumer_secret: TWITTER_API_KEY_SECRET,
+      bearer_token: bearerToken,
     });
 
-    const params = {
-      count: 100,
-      screen_name: screenName,
-      trim_user: true,
-      tweet_mode: 'extended',
-    };
+    // docs https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets#tab2
+    const apiUrl =
+      "https://api.twitter.com/2/users/25073877/tweets?max_results=100&tweet.fields=created_at%2Cpublic_metrics%2Csource&expansions=referenced_tweets.id&media.fields=public_metrics";
 
-    client.get('statuses/user_timeline', params, (error, tweets) => {
-      if (error) return callback({ error, message: messages.fetches.twTimeline }, null);
+    client.get(apiUrl, {}, (error, tweets) => {
+      if (error)
+        return callback({ error, message: messages.fetches.twTimeline }, null);
 
       const newTweets = tweets.reduce((hash, tweet) => {
-        hash[tweet.id_str] = {
+        hash[tweet.id] = {
           date: tweet.created_at,
-          device: tweet.source.split('>')[1].split('<')[0],
-          favorites: tweet.favorite_count,
-          id: tweet.id_str,
+          device: tweet.source,
+          favorites: tweet.public_metrics.like_count || 0,
+          id: tweet.id,
           isDeleted: false,
-          isRetweet: (tweet.full_text || tweet.text).substr(0,3) === 'RT ',
-          retweets: tweet.retweet_count,
-          text: tweet.full_text || tweet.text,
+          isRetweet: !!(
+            tweet.referenced_tweets &&
+            tweet.referenced_tweets.find((t) => t.type === "quoted")
+          ),
+          retweets: tweet.public_metrics.retweet_count || 0,
+          text: tweet.text,
         };
         return hash;
       }, {});
 
       callback(null, Object.values(newTweets));
     });
-
   });
 }
 
