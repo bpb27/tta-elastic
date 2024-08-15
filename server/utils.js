7dcf137fcfd2b184a2dddb7f93d082de2d503e77
Brendan Brown <brendanbrown27@gmail.com>
2024-08-15 10:16:08 -0700

    working server

--- server/utils.js
+++ server/utils.js
@@ -1,19 +1,19 @@
-const tableName = 'trump_tweets';
-const indexName = 'trump_tweets';
+const tableName = "trump_tweets";
+const indexName = "trump_tweets";
 
 const dbString = value => {
-  const ssl = '?ssl=true&sslmode=require';
-  return value.includes(ssl) ? value : `${value}${ssl}`;
+	const ssl = "?ssl=true&sslmode=require";
+	return value.includes(ssl) ? value : `${value}${ssl}`;
 };
 
 const logger = {
-  error: (message, obj) => console.error(message, obj), // eslint-disable-line no-console
-  success: (message, obj) => obj ? console.log(message, obj) : console.log(message), // eslint-disable-line no-console
+	error: (message, obj) => console.error(message, obj), // eslint-disable-line no-console
+	success: (message, obj) => (obj ? console.log(message, obj) : console.log(message)), // eslint-disable-line no-console
 };
 
 module.exports = {
-  dbString,
-  indexName,
-  logger,
-  tableName,
+	dbString,
+	indexName,
+	logger,
+	tableName,
 };
